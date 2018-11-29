5b90350c533e3306c9955d70c6d5db156f58b7e8
daandeheij <daandeheij@gmail.com>
2018-11-29 13:14:57 +0100

    - Added clock functions to javascript - Wrapped rowgenerator in seperate function - Added ID to the clock text html object

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -1,52 +1,63 @@
-// Find the elements with the guesses and solutions ids.
-guesses = document.getElementById('guesses');
-solution = document.getElementById('solution');
+generateRows();
+//saves the current time in seconds (after page load) for the clock
+var startTime = Math.floor(Date.now() / 1000);
+//get the counter object of document that needs to be updated
+var timecounter = document.getElementById("timecounter");
+//call clock function every second
+window.setInterval(updateClock, 1000);
 
-// Declare some properties of the board.
-var numberOfRows = 12;
-var numberOfCodeSlots = 4;
-var numberOfKeySlots = 4;
+function generateRows() {
+    //generates the rows of the board
+    // Find the elements with the guesses and solutions ids.
+    guesses = document.getElementById('guesses');
+    solution = document.getElementById('solution');
 
-// Declare an empty content string, in which the board html will be generated.
-var content = '';
+    // Declare some properties of the board.
+    var numberOfRows = 12;
+    var numberOfCodeSlots = 4;
+    var numberOfKeySlots = 4;
 
-// Generates 12 rows of code and key slots.
-for (var i = 0; i < numberOfRows; i++) {
-    content += '<div class="code-peg-grid">';
+    // Declare an empty content string, in which the board html will be generated.
+    var content = '';
 
-    // Generates 4 columns of code slots.
-    for(var j = 0; j < numberOfCodeSlots; j++){
-        content += '<div class="code-peg-grid-item"><div id="guess-' + i + '-' + j + '" class="code-peg-slot" ondrop="drop(event)" ondragover="allowDrop(event)"></div></div>';
+    // Generates 12 rows of code and key slots.
+    for (var i = 0; i < numberOfRows; i++) {
+        content += '<div class="code-peg-grid">';
+
+        // Generates 4 columns of code slots.
+        for (var j = 0; j < numberOfCodeSlots; j++) {
+            content += '<div class="code-peg-grid-item"><div id="guess-' + i + '-' + j + '" class="code-peg-slot" ondrop="drop(event)" ondragover="allowDrop(event)"></div></div>';
+        }
+
+        content += '<div class="key-peg-grid">';
+
+        // Generates a 2-by-2 grid of key slots.
+        for (var j = 0; j < numberOfKeySlots; j++) {
+            content += '<div class="key-peg-grid-item"><div id="key-' + i + '-' + j + '" class="key-peg-slot"></div></div>';
+        }
+
+        // Close all the divs.
+        content += '</div>';
+        content += '</div>';
     }
 
-    content += '<div class="key-peg-grid">';
+    // Add the generated html to the guesses element.
+    guesses.innerHTML += content;
 
-    // Generates a 2-by-2 grid of key slots.
-    for(var j = 0; j < numberOfKeySlots; j++){
-        content += '<div class="key-peg-grid-item"><div id="key-' + i + '-' + j + '" class="key-peg-slot"></div></div>';
+    // Generate new html for the solution element.
+    content = '<div class="code-peg-grid">';
+
+    // Generate 4 columns of the solution code slots.
+    for (var i = 0; i < numberOfCodeSlots; i++) {
+        content += '<div class="code-peg-grid-item"><div id="solution-' + i + '" class="code-peg-slot"></div></div>';
     }
 
-    // Close all the divs.
+    // Close the div.
     content += '</div>';
-    content += '</div>';
+
+    // Add the generated html to the solution element.
+    solution.innerHTML += content;
 }
-
-// Add the generated html to the guesses element.
-guesses.innerHTML += content;
-
-// Generate new html for the solution element.
-content = '<div class="code-peg-grid">';
-
-// Generate 4 columns of the solution code slots.
-for(var i = 0; i < numberOfCodeSlots; i++){
-    content += '<div class="code-peg-grid-item"><div id="solution-' + i + '" class="code-peg-slot"></div></div>';
-}
-
-// Close the div.
-content += '</div>';
-
-// Add the generated html to the solution element.
-solution.innerHTML += content;
 
 function allowDrop(event) {
     event.preventDefault();
@@ -62,7 +73,7 @@ function drop(event) {
     switch (data) {
         case "color-picker-slot-0":
             event.target.style.backgroundColor = "#ffd08e";
-            keypin(0,0,'white');
+            keypin(0, 0, 'white');
             break;
         case "color-picker-slot-1":
             event.target.style.backgroundColor = "#efc3f5";
@@ -84,15 +95,34 @@ function drop(event) {
     event.preventDefault();
 }
 
-function submit(){
+function submit() {
     console.log("CLICK");
 }
 
-function keypin(row, pin, color){
+function keypin(row, pin, color) {
     var pinToChange = document.getElementById('key-' + row + '-' + pin);
     pinToChange.style.backgroundColor = color;
 }
 
-function setRound(round){
+function setRound(round) {
 
 }
+
+function updateClock() {
+    console.log("updateClock called");
+    var curTime = Math.floor(Date.now() / 1000);
+    var elapsed = curTime - startTime; 
+    console.log(curTime);
+    var minutes = Math.floor(elapsed / 60);
+    var seconds = Math.floor(elapsed % 60);  
+    minutes = prettifyTime(minutes);
+    seconds = prettifyTime(seconds);
+    timecounter.innerHTML = "TIME " + minutes + ":" + seconds;      
+}
+
+//helper function for updateClock. Adds leading zeroes
+function prettifyTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
