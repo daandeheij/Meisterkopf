647cb10cbbf463d10df51298be61de78647debcc
thversfelt <thversfelt@hotmail.com>
2018-11-29 15:21:36 +0000

    Add server-client code.

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -124,3 +124,13 @@ function prettifyTime(i) {
     return i;
 }
 
+var socket = new WebSocket("ws://localhost:3000");
+
+socket.onmessage = function(event){
+    console.log(event.data);
+}
+
+socket.onopen = function(){
+    socket.send("Hello from the client!");
+    console.log("Sending message.");
+};
