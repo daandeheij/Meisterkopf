9b5fe2ef59362b45e83424e8bc746a728396e262
thversfelt <thversfelt@hotmail.com>
2018-11-29 14:54:47 +0000

    Installed websockets and implemented a basic server that can serve the html pages.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res) {
+    res.sendFile("splashscreen.html", {root: "./public"});
+});
+
+/* Pressing the 'PLAY' button, returns this page */
+router.get("/play", function(req, res) {
+    res.sendFile("gamescreen.html", {root: "./public"});
 });
 
 module.exports = router;
