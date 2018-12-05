4550a83684bad6a8e65d63e6eccaa4a41accdc9c
thversfelt <thversfelt@hotmail.com>
2018-12-05 18:24:22 +0000

    Installed the ejs view engine (globally).

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get("/", function (req, res) {
-    res.sendFile("splashscreen.html", {root: "./public"});
+/* GET splash screen. */
+router.get('/', function(req, res) {
+  res.sendFile("splashscreen.html", {root: "./public"});
 });
 
-/* Pressing the 'PLAY' button, returns this page */
-router.get("/play", function(req, res) {
-    res.sendFile("gamescreen.html", {root: "./public"});
+/* GET game screen. */
+router.get('/play', function(req, res) {
+  res.sendFile("gamescreen.html", {root: "./public"});
 });
 
 module.exports = router;
