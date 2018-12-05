92b7ab9b0f689f060b200dafaf0b85bd7dd0df06
daandeheij <daandeheij@gmail.com>
2018-12-05 11:26:51 +0100

    - Client js groundwork

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,25 +1,44 @@
 var playerType = null;
 
+//incoming from server: Client's PlayerType (codemaker/codebreaker)
 function setupStart(announcedPlayerType){
     playerType = announcedPlayerType;
 }
 
+//incoming from server: Codemaker has made a code. Game starts
 function gameStart(){
 
 }
 
+//incoming from server: the codebreaker's guess
 function announceGuess(guess){
 
 }
 
+//incoming from server: the keys for last guess (correct/or in combination)
 function announceKeys(keys)
 {
 
 }
 
+//incoming from server: other client has disconnected. 
 function playerDisconnected()
 {
 
+}
+
+//from this client to server: submit the code (solution)
+function submitCode(code)
+{
+    var message = new Message("submitCode", code);
+    socket.send(message.encode());
+}
+
+//from this client to server: submit this guess
+function submitGuess(code)
+{
+    var message = new Message("submitGuess", code);
+    socket.send(message.encode());
 }
 
 var socket = new WebSocket("ws://localhost:3000");
