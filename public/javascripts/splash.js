671f02ee17e0a01312d601123ca29f15b82b87d3
daandeheij <daandeheij@gmail.com>
2018-12-09 16:54:18 +0100

    - Added a resolution check to splash. Min res gamescreen is 450x540

--- public/javascripts/splash.js
+++ public/javascripts/splash.js
@@ -1,6 +1,10 @@
 
 var menu = new Menu();
 
+if(window.matchMedia("(max-width: 450px)").matches || window.matchMedia("(max-height: 540px)").matches){
+    alert("It seems like your device's resolution is below 800x600. For the best experience, please play Meisterkopf on a device with minimum resolution of 800x600. Or maximize your window ");
+}
+
 function Menu(){
 
     this.play = function(){
